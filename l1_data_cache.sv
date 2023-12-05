af5c0900091315de1b895725997f4f158c2e5f48
suriyasaiyan <72756153+suriyasaiyan@users.noreply.github.com>
2023-12-05 08:37:45 -0500

    ver.2

---
+++ l1_data_cache.sv
@@ -0,0 +1,319 @@
+// Write HIT:  Write-back: In a write-back cache, data is written to the cache and only later to the main memory when the cache line is replaced. 
+// Write MISS: Write allocate: when a write miss occurs, the cache line is loaded into the cache, and then the write operation is performed.
+// Look-through:  which means it checks the main memory for cache misses.
+// LRU (Least Recently Used):  which means the least recently used cache line is selected for replacement.
+
+// ECC, 2 words per block., CACHE CONFLICT, gate clocking
+
+module sram_module(
+    input wire clk,
+    input wire write_enable, read_enable,
+    input wire [8:0] set_index, 
+    input wire way_select,  // to select between the two ways
+    input wire [31:0] write_data, 
+    output reg [31:0] read_data  
+);
+    // Constants for cache configuration
+    localparam BLOCK_SIZE    = 4;      //bytes
+    localparam CACHE_SIZE    = 4 * 1024;  // 4 KB
+    localparam ASSOCIATIVITY = 2;     
+    
+    localparam BLOCK_WIDTH   = BLOCK_SIZE * 8; //32 bits
+    localparam NUM_SETS      = CACHE_SIZE/(BLOCK_SIZE * ASSOCIATIVITY); // 512 sets
+    localparam ADDR_WIDTH    = 32; 
+
+    reg [BLOCK_WIDTH -1:0] memory_array [0:NUM_SETS * ASSOCIATIVITY - 1];
+
+    wire [ADDR_WIDTH -1:0] actual_address = set_index * ASSOCIATIVITY + way_select;
+
+    always @(posedge clk) begin
+        if (write_enable) begin
+//            if (actual_address < (NUM_SETS * ASSOCIATIVITY)) begin
+                memory_array[actual_address] <= write_data;
+        end else if(read_enable) begin
+            read_data = memory_array[actual_address];
+        end
+    end 
+endmodule : sram_module
+
+`timescale 1ns / 1ps
+module L1_Data_Cache(
+    input wire clk,
+    input wire reset,
+    input wire write_enable, read_enable,
+    input wire [31:0] request_address,
+    input wire [31:0] write_data,
+    output reg [31:0] response_data,
+    output reg [1:0] c_state,
+    
+    // To LOWER MEMORY
+    output reg l2_request,
+    output reg l2_write_enable,
+    output reg [31:0] l2_address, 
+    output reg [31:0] l2_write_data,
+    input wire [31:0] l2_response_data,
+    input wire l2_ready
+);
+    // Hardcoded parameters for the data cache
+    localparam CACHE_SIZE    = 4 * 1024;   // Cache size: 4 KB
+    localparam BLOCK_SIZE    = 4;          // Block size: 4 bytes (32 bits)
+    localparam ASSOCIATIVITY = 2;          // 2-way
+    localparam DATA_WIDTH    = 32;
+    
+    localparam BLOCK_WIDTH   = BLOCK_SIZE * 8; //32 bits
+    localparam NUM_SETS      = CACHE_SIZE/(BLOCK_SIZE * ASSOCIATIVITY); // 512 sets
+    localparam ADDR_WIDTH    = 32;                        
+
+    // Calculating the number of bits for offset, index, and tag
+    localparam OFFSET_WIDTH   = $clog2(BLOCK_SIZE);    // 2 bits
+    localparam INDEX_WIDTH    = $clog2(NUM_SETS);       // 9 bits
+    localparam TAG_WIDTH      = ADDR_WIDTH - OFFSET_WIDTH - INDEX_WIDTH;  // 21 bits
+  
+    // Internal Variables
+//    reg [BLOCK_WIDTH- 1:0] cache_data [0:NUM_SETS-1][0:ASSOCIATIVITY-1];
+    //need to add offset access
+    reg [TAG_WIDTH - 1:0] cache_tags [0:NUM_SETS-1][0:ASSOCIATIVITY-1];
+    reg valid [0:NUM_SETS-1][0:ASSOCIATIVITY-1];
+    reg dirty [0:NUM_SETS-1][0:ASSOCIATIVITY-1];
+    reg [ASSOCIATIVITY-1:0] lru_counter [0:NUM_SETS-1];
+    
+    reg hit;
+    reg [ASSOCIATIVITY-1:0] way, lru_way;
+    
+    
+    typedef enum integer {IDLE, CHECK_TAG, WRITEBACK, FILL} cache_state_t;
+    cache_state_t state = IDLE;
+
+    typedef struct packed{
+        logic [31:0] address;
+        logic [TAG_WIDTH-1:0]      tag;
+        logic [INDEX_WIDTH-1:0]    index;
+        logic [OFFSET_WIDTH-1:0]   offset;
+    }current_address_t;
+    current_address_t current_addr;
+    
+    reg [31:0] sram_read_data;
+    typedef struct packed{
+        logic [31:0] write_data;
+        logic write_enable;
+        logic read_enable;
+        logic way;
+        logic [8:0] index;
+    }sram_data_t;
+    sram_data_t put_sram_data;
+ 
+    sram_module cache_data_sram (
+        .clk(clk),
+        .write_enable   (put_sram_data.write_enable),
+        .read_enable    (put_sram_data.read_enable),
+        .set_index      (put_sram_data.index),
+        .way_select     (put_sram_data.way),
+        .write_data     (put_sram_data.write_data),
+        .read_data      (sram_read_data)
+    );
+     
+    // LRU Function
+    function integer get_lru_way(input integer set_index);
+        integer i;
+        reg [ASSOCIATIVITY-1:0] max_count;
+        begin
+            max_count = 0;
+            //max_count = -1; -1 here is 32'hFFFFFFFF turnication happens and max_count will be 3
+            lru_way = 0;
+            for (i = 0; i < ASSOCIATIVITY; i = i + 1) begin            
+                if (lru_counter[set_index][i] > max_count) begin
+                    max_count = lru_counter[set_index][i];
+                    lru_way = i;
+                end
+            end
+            get_lru_way = lru_way;
+        end
+    endfunction
+
+    // Cache Operation Tasks
+    task reset_cache;
+        integer i, j;
+        begin
+            state <= IDLE;
+            for (i = 0; i < NUM_SETS; i = i+1) begin
+                for (j = 0; j < ASSOCIATIVITY; j = j+1) begin
+                
+//                    cache_data[i][j] <= 0;
+                    put_sram_data.write_enable <= 1;
+                    put_sram_data.index <= i;
+                    put_sram_data.way <= j;
+                    put_sram_data.write_data <= 0;
+                    // bring down write_enable
+                    cache_tags[i][j] <= 0;
+                    valid[i][j] <= 0;
+                    dirty[i][j] <= 0;
+                    lru_counter[i][j] <= j;
+                end
+            end
+        end
+    endtask
+
+    task idle_state_logic;
+        begin
+            if (write_enable || read_enable) begin
+                current_addr.address <= request_address;
+                current_addr.tag     <= request_address[ADDR_WIDTH-1 -: TAG_WIDTH];
+                current_addr.index   <= request_address[(ADDR_WIDTH - TAG_WIDTH - 1)-: INDEX_WIDTH];
+                current_addr.offset  <= request_address[OFFSET_WIDTH-1 -: OFFSET_WIDTH];                      
+                state <= CHECK_TAG;
+            end
+        end
+    endtask
+
+    task check_tag_logic;
+        integer i;
+        begin
+            hit = 0;
+            // have to parallel check for better performance
+            lru_way = get_lru_way(current_addr.index); // new
+            for (i = 0; i < ASSOCIATIVITY; i = i + 1) begin
+                if (valid[current_addr.index][i] && cache_tags[current_addr.index][i] == current_addr.tag) begin
+                    hit = 1;
+                    way = i;                
+                    break;
+                end
+            end
+            if (hit) begin
+                handle_cache_hit();
+            end else begin
+                handle_cache_miss();
+            end
+        end
+    endtask
+
+    task handle_cache_hit;
+        begin
+            if (write_enable) begin
+//                cache_data[current_addr.index][way] <= write_data;
+                
+                put_sram_data.write_enable <=1;
+                put_sram_data.index <= current_addr.index;
+                put_sram_data.way <= way;
+                put_sram_data.write_data <= write_data;
+                
+                // need to get down the put_sram_data.write_enable low?
+                
+                dirty[current_addr.index][way] <= 1;
+                state <= IDLE;
+            end else if (read_enable) begin
+            
+//                response_data <= cache_data[current_addr.index][way];
+                put_sram_data.read_enable <=1;
+                put_sram_data.index <= current_addr.index;
+                put_sram_data.way <= way;
+                response_data <= sram_read_data;
+                //do we need to make read_enable low here?
+                state <= IDLE;
+            end
+            update_lru_counters(current_addr.index, way);
+        end
+    endtask
+
+    task handle_cache_miss;
+        begin
+            if (dirty[current_addr.index][lru_way]) begin
+                state <= WRITEBACK;
+            end else begin
+                state <= FILL;
+            end
+        end
+    endtask
+
+    task writeback_logic;
+        begin
+            l2_request <= 1;
+            l2_address <= current_addr.address;
+            l2_write_enable <= 1;
+            
+//            l2_write_data <= cache_data[current_addr.index][lru_way];
+            put_sram_data.read_enable <=1;
+            put_sram_data.index <= current_addr.index;
+            put_sram_data.way <= lru_way;
+            l2_write_data <= sram_read_data;
+    
+            if (l2_ready) begin
+                l2_write_enable <= 0;
+                l2_request <=0;
+                state <= FILL; 
+            end 
+//            else begin
+//            state <= FILL; // Directly proceed to fill if the line is not dirty
+//            end
+        end
+    endtask
+    
+    task fill_logic;
+        begin  
+            l2_request <= 1;
+            l2_address <= current_addr.address;
+            if (l2_ready) begin
+            
+//                cache_data[current_addr.index][lru_way] <= l2_response_data;              
+                put_sram_data.write_enable <=1;
+                put_sram_data.index <= current_addr.index;
+                put_sram_data.way <= lru_way;
+                put_sram_data.write_data <= l2_response_data;
+                
+                cache_tags[current_addr.index][lru_way] <= current_addr.tag;
+                valid[current_addr.index][lru_way] <= 1;
+                dirty[current_addr.index][lru_way] <= 0; 
+    
+                update_lru_counters(current_addr.index, lru_way);
+    
+                l2_request <= 0; 
+                state <= CHECK_TAG; 
+            end
+            // Similar to writeback_logic, consider adding handling for when l2_ready is low
+        end
+    endtask
+
+    task update_lru_counters(input integer set_index, input integer accessed_way);
+        integer i;
+        begin
+            for (i = 0; i < ASSOCIATIVITY; i = i + 1) begin
+                if (i == accessed_way) begin
+                    lru_counter[set_index][i] <= 0;
+                end else if (lru_counter[set_index][i] != (ASSOCIATIVITY - 1)) begin
+                    lru_counter[set_index][i] <= lru_counter[set_index][i] + 1;
+                end
+            end
+        end
+    endtask
+
+    // Main Cache Operation 
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            reset_cache();
+        end else begin
+            case (state)
+                IDLE: begin
+                    idle_state_logic();
+                    c_state <= 0;
+                end
+                CHECK_TAG: begin
+                    check_tag_logic();
+                    c_state <= 1; 
+                end
+                WRITEBACK: begin
+                    writeback_logic();
+                    c_state <= 2; 
+                end
+                FILL: begin
+                    fill_logic();
+                    c_state <= 3; 
+                end
+                default: begin
+                    idle_state_logic();
+                    c_state <= 0; 
+                end
+            endcase
+        end
+    end
+    
+endmodule
+
---
+++ tb_l1_data_cache.sv
@@ -0,0 +1,219 @@
+`timescale 1ns / 1ps
+
+module tb_L1_Data_Cache;
+    parameter CLOCK_PERIOD   = 10;    
+    reg clk;
+    reg reset;
+    reg write_enable;
+    reg read_enable;
+    reg [31:0] request_address;
+    reg [31:0] write_data;
+    wire [31:0] response_data;
+    wire [1:0] c_state;
+
+    // L2 Cache 
+    reg [31:0] l2_response_data;
+    reg l2_ready;
+    reg l2_request;
+    reg l2_write_enable;
+    wire [31:0] l2_address;
+    wire [31:0] l2_write_data;
+    
+    // Hardcoded parameters for the data cache
+    localparam L2_CACHE_SIZE    = 16 * 1024;   // Cache size: 4 KB
+    localparam L2_BLOCK_SIZE    = 4;          // Block size: 4 bytes (32 bits)
+    localparam L2_ASSOCIATIVITY = 4;          // 2-way
+    localparam L2_DATA_WIDTH    = 32;
+    
+    localparam L2_BLOCK_WIDTH   = L2_BLOCK_SIZE * 8;
+    localparam L2_NUM_SETS      = L2_CACHE_SIZE/(L2_BLOCK_SIZE * L2_ASSOCIATIVITY);
+    localparam L2_ADDR_WIDTH    = 32;                        
+
+    // Calculating the number of bits for offset, index, and tag
+    localparam L2_OFFSET_WIDTH   = $clog2(L2_BLOCK_SIZE);    
+    localparam L2_INDEX_WIDTH    = $clog2(L2_NUM_SETS);        
+    localparam L2_TAG_WIDTH      = L2_ADDR_WIDTH - L2_OFFSET_WIDTH - L2_INDEX_WIDTH; 
+    
+    // L2 Cache Address Decomposition
+    reg [L2_TAG_WIDTH-1:0]      current_tag;
+    reg [L2_INDEX_WIDTH-1:0]    current_index;
+    reg [L2_OFFSET_WIDTH-1:0]   current_offset;
+
+    L1_Data_Cache dut (
+        .clk(clk),
+        .reset(reset),
+        .write_enable(write_enable),
+        .read_enable(read_enable),
+        .request_address(request_address),
+        .write_data(write_data),
+        .response_data(response_data),
+        .c_state(c_state),
+        
+        .l2_response_data(l2_response_data),
+        .l2_ready(l2_ready),
+        .l2_request(l2_request),
+        .l2_write_enable(l2_write_enable),
+        .l2_address(l2_address),
+        .l2_write_data(l2_write_data)      
+    );
+    
+    // L2 Cache Data Storage
+    reg [L2_BLOCK_WIDTH-1:0] l2_cache_data [0: L2_NUM_SETS-1][0: L2_ASSOCIATIVITY-1];
+    reg [L2_TAG_WIDTH - 1:0] l2_cache_tags [0:L2_NUM_SETS-1][0:L2_ASSOCIATIVITY-1];
+    
+    initial begin
+
+       l2_cache_data [2][0] <= 32'hAAAAAAAA;
+       l2_cache_data [2][1] <= 32'hBBBBBBBB;
+       l2_cache_data [2][2] <= 32'hCCCCCCCC;
+       l2_cache_data [2][3] <= 32'hDDDDDDDD;
+        
+       l2_cache_data [524][0] <= 32'hEEEEEEEE;
+       l2_cache_data [524][1] <= 32'hFFFFFFFF;
+       l2_cache_data [524][2] <= 32'hABCDEFAB;
+       l2_cache_data [524][3] <= 32'hFEDCBAFE;
+
+       l2_cache_tags [2][0] <= 20'h00001;
+       l2_cache_tags [2][1] <= 20'h00002;
+       l2_cache_tags [2][2] <= 20'h00003;
+       l2_cache_tags [2][3] <= 20'h00004;
+        
+       l2_cache_tags [524][0] <= 20'h00000;        // 32'h00000832
+       l2_cache_tags [524][1] <= 20'hABCDE;        // 32'hABCDE832
+       l2_cache_tags [524][2] <= 20'hAAAAA;        // 32'hAAAAA832
+       l2_cache_tags [524][3] <= 20'hFFFFF;        // 32'hFFFFF832
+    end
+
+    initial begin
+        clk =0;
+        forever #(CLOCK_PERIOD/2) clk = ~clk;
+    end
+
+    initial begin
+        reset_signals();
+        
+        read_enable =1;  
+        request_address = 32'h00000832;
+        #61 read_enable = 0; #9;  // fill and read 
+        
+        #10 read_enable = 1;  
+        request_address = 32'hABCDE832;
+        #61 read_enable = 0; #9;
+        
+        test_cache_read_hit (32'h00000832, 32'hEEEEEEEE);
+        test_cache_read_hit (32'hABCDE832, 32'hFFFFFFFF);
+        
+        test_cache_read_miss(32'hAAAAA832, 32'hABCDEFAB); // new
+        
+        test_cache_read_hit (32'hABCDE832, 32'hFFFFFFFF);
+        test_cache_read_miss(32'h00000832, 32'hEEEEEEEE); // new
+        test_cache_write_hit(32'hABCDE832, 32'hFFFFFFFF);
+        test_cache_read_miss(32'hAAAAA832, 32'hABCDEFAB);
+        test_cache_read_writeback_miss(32'hFFFFF832, 32'hFEDCBAFE);
+    end
+
+    task reset_signals;
+        begin
+            reset = 1;
+            write_enable = 0;
+            read_enable = 0;
+            l2_ready = 0;
+            l2_request = 0;
+            l2_write_enable = 0;
+            write_data = 0;
+            l2_response_data = 0;
+            
+            #15 reset = 0; 
+        end
+    endtask
+
+    task test_cache_read_hit(input [L2_ADDR_WIDTH-1:0] addr, input [31:0] expected_data);
+        begin
+            #10 read_enable = 1;
+            request_address = addr;
+
+            #31 read_enable = 0;
+            assert(response_data == expected_data) else $display("Read Hit Test Failed for address %h", addr);
+            #9;
+        end
+    endtask
+    
+    task test_cache_read_writeback_miss(input[L2_ADDR_WIDTH-1:0] addr, input [31:0] expected_data);
+        begin
+            #10 read_enable = 1;
+            request_address = addr;
+            
+            #81 read_enable = 0;
+            assert(response_data == expected_data) else $display("Read Miss Test Failed for address %h", addr);
+            #9;
+        end
+    endtask
+
+    task test_cache_write_hit(input [L2_ADDR_WIDTH-1:0] addr, input [31:0] data);
+        begin
+            #10 write_enable = 1;
+            request_address = addr;
+            write_data = data;         
+            #31 write_enable = 0; #9;         
+        end
+    endtask
+
+    task test_cache_read_miss(input [L2_ADDR_WIDTH-1:0] addr, input [31:0] expected_data);
+        begin
+            #10 read_enable = 1;
+            request_address = addr;
+            #61; 
+            read_enable = 0;
+            assert(response_data == expected_data) else $display("Read Miss Test Failed for address %h", addr);
+            #9;
+        end
+    endtask
+
+    task test_cache_write_miss(input [L2_ADDR_WIDTH-1:0] addr, input [31:0] data);
+        begin
+            write_enable = 1;
+            request_address = addr;
+            write_data = data;
+            #41; 
+            write_enable = 0;
+        end
+    endtask
+       
+    // L2 Cache Simulation Logic
+    always @(posedge clk) begin
+        integer i;
+        reg hit;
+        reg [31:0] selected_data;
+        
+        if (l2_request && !l2_ready) begin
+
+            current_offset = l2_address[L2_OFFSET_WIDTH-1 -: L2_OFFSET_WIDTH];
+            current_index  = l2_address[(L2_ADDR_WIDTH - L2_TAG_WIDTH - 1)-: L2_INDEX_WIDTH];
+            current_tag    = l2_address[L2_ADDR_WIDTH-1 -: L2_TAG_WIDTH]; 
+            
+            hit = 0;
+            for (i = 0; i < L2_ASSOCIATIVITY; i = i + 1) begin
+                if (l2_cache_tags[current_index][i] == current_tag) begin
+                
+                    hit = 1;
+                    selected_data = l2_cache_data[current_index][i];
+                    break;
+                end
+            end
+    
+            if (hit) begin
+                if (l2_write_enable) begin
+//                    l2_cache_data[current_index][i] = l2_write_data;
+                end else begin
+                    l2_response_data = selected_data;
+                end
+                l2_ready = 1;
+                #10 l2_ready = 0;
+            end else begin
+                l2_response_data = 32'hdeadbeef; 
+                l2_ready = 1;
+                #10 l2_ready = 0;
+                end
+            end
+    end
+endmodule
